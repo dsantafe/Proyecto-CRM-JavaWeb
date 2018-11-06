d4fa2db625c48a6b6baa5147a6bf0ce5a62ecdff
David Santafe Zorrilla <santafedavid01@gmail.com>
2018-11-06 12:34:15 -0500

    Initial commit

---
+++ src/java/BL/clsCalificaciones.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BL;
+
+import java.sql.*;
+import java.util.*;
+
+/**
+ *
+ * @author David
+ */
+public class clsCalificaciones {
+    
+    Connection conn = null;
+    
+    public clsCalificaciones(){
+        clsConexion obclsConexion = new clsConexion();
+        conn = obclsConexion.getConexion();
+    }
+    
+    public List<Models.clsCalificacion> getCalificacion(){
+        
+        List<Models.clsCalificacion> lstclsCalificacion = new ArrayList<Models.clsCalificacion>();
+        
+        try{
+            
+            PreparedStatement ps = conn.prepareStatement("{ call spConsultarCalificacion() }");
+            ResultSet rs = ps.executeQuery();
+            
+            while(rs.next()){
+                Models.clsCalificacion obclsCalificacion = new Models.clsCalificacion();
+                obclsCalificacion.setInCodigo(rs.getInt("caliCodigo"));
+                obclsCalificacion.setStDescripion(rs.getString("caliDescripcion"));
+                
+                lstclsCalificacion.add(obclsCalificacion);
+            }
+        
+        }catch(Exception ex){}
+        
+        return lstclsCalificacion;
+    }
+}
